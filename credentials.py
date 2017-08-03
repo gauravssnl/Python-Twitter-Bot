5d7fd32e8166ba70f1d5b0d4e4ffff68268233c3
GAURAV SINGH <Gaurav.ssnl@gmail.com>
2017-08-04 01:49:31 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,6 @@
+#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
