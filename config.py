1ccaadc621de4c8a8a9283da5b4ae90a05348711
GAURAV SINGH <Gaurav.ssnl@gmail.com>
2017-08-04 20:57:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Edit this confif file as you like
+
+# This is hastag which Twitter bot will search and retweet. You can edit this with any hastag .For example : '#javascript'
+
+QUERY = '#python'
+
+# Twitter bot setting for liking Tweets
+LIKE = True 
+
+# Twitter bot setting for following user who tweeted
+FOLLOW = False
+
+# Twitter bot sleep time settings in seconds. For example SLEEP_TIME = 300 means 5 minutes.
+# you can decrease it or increase it as you like.Please,use large delay if you are running bot all the time  so that your account does not get banned.
+
+SLEEP_TIME = 300
--- twitterbot_retweet.py
+++ twitterbot_retweet.py
@@ -1,10 +1,16 @@
 import tweepy
 from time import sleep
 from credentials import *
+from config import QUERY, FOLLOW, LIKE, SLEEP_TIME
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+print("Twitter bot which retweets,like tweets and follow users")
+print("Bot Settings")
+print("Like Tweets :", LIKE)
+print("Follow users :", FOLLOW)
 
 for tweet in tweepy.Cursor(api.search, q='#python').items():
     try:
@@ -14,16 +20,18 @@ for tweet in tweepy.Cursor(api.search, q='#python').items():
         print('Retweeted the tweet')
 
         # Favorite the tweet
-        tweet.favorite()
-        print('Favorited the tweet')
+        if LIKE:
+            tweet.favorite()
+            print('Favorited the tweet')
 
         # Follow the user who tweeted
         #check that bot is not already following the user
-        if not tweet.user.following:
-            tweet.user.follow()
-            print('Followed the user')
+        if FOLLOW:
+            if not tweet.user.following:
+                tweet.user.follow()
+                print('Followed the user')
 
-        sleep(5)
+        sleep(SLEEP_TIME)
 
     except tweepy.TweepError as e:
         print(e.reason)
